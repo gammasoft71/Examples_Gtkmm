9efb5556d2c86392133267f984cacf637774f88e
gammasoft71 <gammasoft71@gmail.com>
2018-06-07 12:39:13 +0200

    add TextBox

---
+++ src/TextBox/README.md
@@ -0,0 +1,5 @@
+# TextBox
+
+This example demonstrates the use of Gtk::Entry control.
+
+![GitHub Logo](../../docs/Pictures/TextBox.png)
---
+++ src/TextBox/TextBox.cpp
@@ -0,0 +1,33 @@
+#include <gtkmm.h>
+
+using namespace Glib;
+using namespace Gtk;
+
+class Form : public Window {
+public:
+  Form() {
+    this->add(this->scrolledWindow);
+    this->scrolledWindow.add(this->fixed);
+    
+    this->textBox1.set_text("textBox1");
+    this->fixed.add(this->textBox1);
+    this->fixed.child_property_x(this->textBox1) = 10;
+    this->fixed.child_property_y(this->textBox1) = 10;
+    
+    this->set_title("Label example");
+    this->set_size_request(300, 300);
+    this->show_all();
+  }
+  
+private:
+  Fixed fixed;
+  ScrolledWindow scrolledWindow;
+  Entry textBox1;
+};
+
+// The main entry point for the application.
+int main(int argc, char* argv[]) {
+  RefPtr<Application> application = Application::create(argc, argv);
+  Form form;
+  return application->run(form);
+}
