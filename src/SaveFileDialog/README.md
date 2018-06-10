37435eeac6d840fc1376e3791c8ae9bdd5bf9c6e
gammasoft71 <gammasoft71@gmail.com>
2018-06-10 23:43:58 +0200

    add SaveFileDialog

---
+++ src/SaveFileDialog/README.md
@@ -0,0 +1,5 @@
+# SaveFileDialog
+
+This example demonstrates the use of Gtk::FileChooserDialog dialog.
+
+![GitHub Logo](../../docs/Pictures/SaveFileDialog.png)
---
+++ src/SaveFileDialog/SaveFileDialog.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <gtkmm.h>
+
+using namespace Glib;
+using namespace Gtk;
+
+class Form : public Window {
+public:
+  Form() {
+    this->add(this->scrolledWindow);
+    this->scrolledWindow.add(this->fixed);
+    
+    this->button.set_label("Save...");
+    this->fixed.add(this->button);
+    this->fixed.child_property_x(this->button) = 10;
+    this->fixed.child_property_y(this->button) = 10;
+    this->button.signal_button_release_event().connect([&](GdkEventButton* event) {
+      FileChooserDialog saveFileDialog("", FILE_CHOOSER_ACTION_SAVE);
+      saveFileDialog.add_button("Cancel", RESPONSE_CANCEL);
+      saveFileDialog.add_button("Open", RESPONSE_OK);
+      saveFileDialog.set_current_folder(ustring::compose("%1/Desktop", ustring(getenv("HOME"))));
+      saveFileDialog.set_current_name("MyFile.txt");
+      saveFileDialog.set_transient_for(*this);
+
+      Glib::RefPtr<Gtk::FileFilter> fileFilter = Gtk::FileFilter::create();
+      fileFilter->set_name("Text Files (*.txt)");
+      fileFilter->add_pattern("*.txt");
+      saveFileDialog.add_filter(fileFilter);
+      fileFilter = Gtk::FileFilter::create();
+      fileFilter->set_name("All Files (*.*)");
+      fileFilter->add_pattern("*.*");
+      saveFileDialog.add_filter(fileFilter);
+      
+      if (saveFileDialog.run() == RESPONSE_OK)
+        this->label.set_text(ustring::compose("File = %1", ustring(saveFileDialog.get_filename())));
+      return true;
+    });
+
+    this->label.set_text("File = ");
+    this->fixed.add(this->label);
+    this->fixed.child_property_x(this->label) = 10;
+    this->fixed.child_property_y(this->label) = 50;
+
+    this->set_title("SaveFileDialog example");
+    this->set_size_request(300, 300);
+    this->show_all();
+  }
+  
+private:
+  Fixed fixed;
+  ScrolledWindow scrolledWindow;
+  Button button;
+  Label label;
+};
+
+int main(int argc, char* argv[]) {
+  RefPtr<Application> application = Application::create(argc, argv);
+  Form form;
+  return application->run(form);
+}
