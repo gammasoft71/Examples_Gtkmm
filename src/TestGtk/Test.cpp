8c99169332e68ee7f0aa4716e6451b9b86a3edc4
Gammasoft71 <gammasoft71@gmail.com>
2018-07-28 23:39:36 +0200

    add Text Gtk

--- src/TestGtk/Test.cpp
+++ src/TestGtk/Test.cpp
@@ -1,10 +1,16 @@
-#include <gtk/gtk.h>
+#include <memory>
+#include <gtkmm.h>
 
 void OnButtonClicked(GtkButton* button, gpointer application) {
   g_application_quit(G_APPLICATION(application));
 }
 
 void OnApplicationActivate(GtkApplication* application, gpointer userData) {
+  GtkRequestedSize requestedSize;
+  requestedSize.minimum_size = 0;
+  requestedSize.natural_size = 0;
+  gtk_distribute_natural_allocation(0, 0, &requestedSize);
+
   GtkWindow* form = GTK_WINDOW(gtk_window_new(GTK_WINDOW_TOPLEVEL));
   gtk_window_set_title(form, "Test");
   gtk_window_set_default_size(form, 300, 300);
@@ -13,12 +19,12 @@ void OnApplicationActivate(GtkApplication* application, gpointer userData) {
   gtk_container_add(GTK_CONTAINER(form), GTK_WIDGET(fixed));
 
   GtkButton* button = GTK_BUTTON(gtk_button_new());
-  GTK_WIDGET_GET_CLASS(button)->get_preferred_width = [](GtkWidget* widget, gint *minimum_width, gint *natural_width) {*minimum_width = *natural_width = 0;};
-  GTK_WIDGET_GET_CLASS(button)->get_preferred_height_and_baseline_for_width = [](GtkWidget* widget, gint width, gint* minimum_height, gint* natural_height,gint* minimum_baseline, gint* natural_baseline) {*minimum_height = *natural_height = *minimum_baseline = *natural_baseline = 0;};
   gtk_button_set_label(button, "Quit");
+  gtk_widget_set_size_request(GTK_WIDGET(button), 50, 10);
+  gtk_widget_set_hexpand(GTK_WIDGET(button), false);
+  gtk_widget_set_vexpand(GTK_WIDGET(button), false);
   gtk_container_add(GTK_CONTAINER(fixed), GTK_WIDGET(button));
   gtk_fixed_move(fixed, GTK_WIDGET(button), 10, 10);
-  gtk_widget_set_size_request(GTK_WIDGET(button), 75, 25);
   g_signal_connect(button, "clicked", G_CALLBACK(OnButtonClicked), application);
 
   gtk_widget_show_all(GTK_WIDGET(form));
